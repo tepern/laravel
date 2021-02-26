3fa22dfd3b66672fb0aee205b57c5e5d91f189dd
Тепер Наталья <n.teper@itgroup.local>
2021-02-26 15:22:41 +0400

    Base Controller and Blog Post Controller
    Вывод всех статей блога

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function() {
+    Route::resource('posts','PostController')->names('blog.posts');
+});
+
 Route::resource('rest', RestTestController::class);
