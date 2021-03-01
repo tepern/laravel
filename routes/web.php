5fbf421fccd167e41232fc3797fcc9887facac2e
Тепер Наталья <n.teper@itgroup.local>
2021-03-01 11:27:52 +0400

    Admin: Categories

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//>Админка Блога
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function() {
+    //BlogCategory
+    $methods = ['index', 'edit', 'store', 'update', 'create',];
+    Route::resource('categories','CategoryController')->only($methods)->names('blog.admin.categories');
+});
